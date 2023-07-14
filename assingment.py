5f6904e15a996a413a8319e1ebecad20c4f9bfcb
Mihir <mihirnigam24@gmail.com>
2023-07-14 09:01:52 +0530

    first commit

---
+++ assingment.py
@@ -0,0 +1,11 @@
+number = int(input("number is....."))
+factorial =1 
+if number < 0:
+    print (".......")
+    elif number == 0 :
+        print("0>1")
+        else:
+            for i is range(1, number +1 ):
+                factorial *= i
+                print(factorial)
+            
